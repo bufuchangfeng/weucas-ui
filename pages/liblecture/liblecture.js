c55fb05db944781524f74761e014f085b403478f
yuchen <924761163@qq.com>
2019-11-14 21:15:40 +0800

    添加新的页面

--- pages/liblecture/liblecture.js
+++ pages/liblecture/liblecture.js
@@ -12,17 +12,95 @@ Page({
     list: [{"name":"未开始",index:0},
            {"name":"进行中",index:1},
            {"name":"已结束",index:2}],
-    lecturelist:[]
+    lecturelist: [],
+    latitude: null,
+    longitude: null
   },
+
+  setList:function(){
+    var that = this
+    // console.log(this.data.list[this.data.TabCur].name)
+    var openid = wx.getStorageSync("openid")
+     wx.request({
+          data: util.json2Form({
+            openid: openid,
+            status: this.data.list[this.data.TabCur].name
+          }),
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          url: 'https://www.neumark.top/liblecture/getLectures',
+          method: "POST",
+          success: (res) => {
+            // console.log(res.data)
+            that.setData({
+              lecturelist: res.data
+            })
+          }
+     })
+  },
+
+  onLoad:function(){
+    this.setList()
+  },
+
   tabSelect(e) {
     // console.log(e.currentTarget.dataset)
     this.setData({
       TabCur: e.currentTarget.dataset.id,
       scrollLeft: (e.currentTarget.dataset.id-1)*60
     })
-    console.log(this.data.list[this.data.TabCur].name)
+    this.setList()
   },
 
+  register:function(e){
+    // console.log(e.currentTarget.dataset.liblectureid)
+    var that = this
+    var openid = wx.getStorageSync("openid")
+
+  qq.getLocation({
+    type: 'wgs84',
+    success(res) {
+      that.setData({
+        latitude : res.latitude,
+        longitude : res.longitude
+      })
+      // console.log(res.latitude)
+      // console.log(res.longitude)
+      //  wx.showToast({
+      //           title: res.latitude + " " + res.longitude,
+      //           icon: "none",
+      //            duration: 2000
+      // })
+         wx.request({
+          data: util.json2Form({
+            openid: openid,
+            liblectureid: e.currentTarget.dataset.liblectureid,
+            latitude: res.latitude,
+            longitude: res.longitude
+          }),
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          url: 'https://www.neumark.top/liblecture/register',
+          method: "POST",
+          success: (res) => {
+            // console.log(res.data)
+          }
+     })
+      }
+    })
+  
+   
+  },
+
+
+  showCardView: function(e){
+    // console.log(e.currentTarget.dataset.index)
+    wx.navigateTo({
+      url: '/pages/liblectureinfo/liblectureinfo?liblecture=' + JSON.stringify(this.data.lecturelist[e.currentTarget.dataset.index])
+    })
+  },
 
 
 
--- pages/liblectureinfo/liblectureinfo.js
+++ pages/liblectureinfo/liblectureinfo.js
@@ -1,27 +1,27 @@
-// pages/score/score.js
-var util = require("../../utils/util.js")
-
+// pages/liblectureinfo/liblectureinfo.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    scores:{}
+    liblecture: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getScores()
+    this.setData({
+      liblecture: JSON.parse(options.liblecture)
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
@@ -64,37 +64,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-
-
-  getScores:function(){
-
-    var openid = wx.getStorageSync("openid")
-    if(openid == ""){
-       wx.showToast({
-        title: '请先登录！',
-        icon: "none",
-        duration: 2000
-      }) 
-      return
-    }
-
-     var that = this
-     wx.request({
-          data: util.json2Form({
-            openid: openid,
-          }),
-          header: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          url: 'https://www.neumark.top/user/get_scores',
-          method: "POST",
-          success: (res) => {
-            // console.log(res.data)
-            that.setData({
-              scores: res.data
-            })
-      }
-    })
   }
 })
